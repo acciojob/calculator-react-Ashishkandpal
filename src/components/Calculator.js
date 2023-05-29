fee05cf0a59d4c9836f3b367cbc3d4cc9e47ce1d
Ashish Kandpal <63722194+Ashishkandpal@users.noreply.github.com>
2023-05-29 20:42:19 +0530

    submitting code-Mon May 29 2023 20:42:19 GMT+0530 (India Standard Time)

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -24,26 +24,12 @@ const Calculator = () => {
       <input type="text" value={result} />
 
       <div className="keypad">
-        <button name="1" onClick={handleClick}>
-          1
+        <button onClick={clear}>C</button>
+        <button name="/" onClick={handleClick}>
+          /
         </button>
-        <button name="2" onClick={handleClick}>
-          2
-        </button>
-        <button name="3" onClick={handleClick}>
-          3
-        </button>
-        <button name="+" onClick={handleClick}>
-          +
-        </button>
-        <button name="4" onClick={handleClick}>
-          4
-        </button>
-        <button name="5" onClick={handleClick}>
-          5
-        </button>
-        <button name="6" onClick={handleClick}>
-          6
+        <button name="*" onClick={handleClick}>
+          *
         </button>
         <button name="-" onClick={handleClick}>
           -
@@ -57,20 +43,36 @@ const Calculator = () => {
         <button name="9" onClick={handleClick}>
           9
         </button>
-        <button name="*" onClick={handleClick}>
-          *
+        <button name="+" onClick={handleClick}>
+          +
         </button>
-        <button name="." onClick={handleClick}>
-          .
+        <button name="4" onClick={handleClick}>
+          4
         </button>
+        <button name="5" onClick={handleClick}>
+          5
+        </button>
+        <button name="6" onClick={handleClick}>
+          6
+        </button>
+
+        <button name="1" onClick={handleClick}>
+          1
+        </button>
+        <button name="2" onClick={handleClick}>
+          2
+        </button>
+        <button name="3" onClick={handleClick}>
+          3
+        </button>
+        <button onClick={calculate}>=</button>
         <button name="0" onClick={handleClick}>
           0
         </button>
-        <button name="/" onClick={handleClick}>
-          /
+
+        <button name="." onClick={handleClick}>
+          .
         </button>
-        <button onClick={clear}>Clear</button>
-        <button onClick={calculate}>=</button>
       </div>
     </div>
   );
